618661df528c7ca77947756c3d2a88ff7b680a2f
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-26 19:23:09 -0300

    Crea conexión con la app de firebase

---
+++ scripts/service/firebase.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyABb5yG5CU6G2tpe7S4Fw3q-JJp7HsWGZo",
+    authDomain: "todolist-cilsa.firebaseapp.com",
+    projectId: "todolist-cilsa",
+    storageBucket: "todolist-cilsa.appspot.com",
+    messagingSenderId: "26715325526",
+    appId: "1:26715325526:web:6223cc2c40d114338667f7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
