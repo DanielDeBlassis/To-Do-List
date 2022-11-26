a85200d46a19c65c976437e146a88ffb2707d462
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-26 19:49:19 -0300

    Crea función para listar tareas almacenadas
    La misma escucha el evento de carga del DOM, y de manera asíncrona obtiene todas las tareas almacenadas en la base de datos de firestore.
    Se iteran las mismas y se formatean como código HTML, completando con la información específica para cada una, para luego insertar como nuevos elementos "li" dentro del contenedor "ul" de tareas de la página. Se le agregan además las clases para estilizado de tailwind.
    Además se agregan botones de edición y borrado con el ID de la tarea correspondiente.

--- app.js
+++ app.js
@@ -29,3 +29,39 @@ $botonCerrar.addEventListener("click", () => {
     taskForm.reset();
 })
 
+window.addEventListener("DOMContentLoaded", async () => {
+
+    onGetTasks((tasks) => {
+
+        $listaDeTareas.innerHTML = "";
+
+        tasks.forEach(doc => {
+            const task = doc.data();
+
+            const tareaLi = document.createElement("li");
+            tareaLi.classList.add("group", `bg-[${task.colorNota}]`, "rounded-md", "p-3", "ring-1", "ring-slate-200", "shadow-sm", "hover:shadow-md");
+            tareaLi.innerHTML = `
+        <dl class="relative w-full h-full flex flex-col justify-between content-between gap-2">
+            <div class="absolute right-0">
+                <img class="w-9 " src="./assets/images/pushpin.svg"/>
+            </div>
+            <div>
+                <dt class="sr-only">Title</dt>
+                <dd class="font-['Exo_2'] font-semibold text-2xl text-[${task.colorTexto}]">${task.titulo}</dd>
+            </div>
+            <div>
+                <dt class="sr-only">Description</dt>
+                <dd class="font-['Patrick_Hand'] text-[${task.colorTexto}]">${task.descripcion}</dd>
+            </div>
+            <div class="w-full gap-1 mt-2 p-2 flex justify-between">
+                <button class="btn-delete rounded w-1/2 h-10 bg-gray-400/50 hover:bg-[#Ff0003] flex justify-center content-center items-center" title="Borrar" data-id="${doc.id}"><i class="fas fa-trash text-[#0000000] text-base group-hover:text-[white]" data-id="${doc.id}"></i></button>
+                <button class="btn-edit rounded w-1/2 h-10 bg-gray-400/50 hover:bg-[#138713] flex justify-center content-center items-center" title="Editar" data-id="${doc.id}"><i class="fas fa-pen text-[#000000] text-base group-hover:text-[white]" data-id="${doc.id}"></i></button>
+            </div>
+        </dl>
+        `;
+            $listaDeTareas.appendChild(tareaLi);
+        });
+
+
+
+});
