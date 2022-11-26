e52f31b35e005330a5fa9eb1545d42477c98d89f
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-26 19:24:31 -0300

    Crea y exporta funciones CRUD firestore

--- scripts/service/firestore.js
+++ scripts/service/firestore.js
@@ -14,3 +14,17 @@ import {
 app;
 const db = getFirestore();
 
+export const saveTask = (titulo, descripcion, colorNota, colorTexto) => {
+    addDoc(collection(db, "tareas"), { titulo, descripcion, colorNota, colorTexto })
+}
+
+export const getTasks = () => getDocs(collection(db, "tareas"));
+
+export const onGetTasks = (callback) => onSnapshot(collection(db, "tareas"), callback);
+
+export const deleteTask = (id) => deleteDoc(doc(db, "tareas", id));
+
+export const getTask = (id) => getDoc(doc(db, "tareas", id));
+
+export const updateTask = (id, newFields) => updateDoc(doc(db, "tareas", id), newFields);
+
