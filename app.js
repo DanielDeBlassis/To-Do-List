2eae9db91df345e0bd4c3257f932d9558fe9ece7
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2022-11-26 19:36:58 -0300

    Obtiene elementos de la página
    Corresponden al formulario, al título del modal desde el cual se edita y crean nuevas tareas; también los botones para añadir una nueva tarea, para cerrar el modal y el contenedor desde el cual se visualizarán la lista de todas las tareas almacenadas hasta el momento.

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 import { saveTask, onGetTasks, deleteTask, getTask, updateTask } from "./scripts/service/firestore.js"
 
+const taskForm = document.getElementById("task-form");
+const $tituloToDo = document.getElementById("titulo-hacer");
+const $botonNuevaTarea = document.querySelector("#btn-nueva-tarea");
+const $listaDeTareas = document.querySelector("#alt-container");
+
+const $botonAgregar = document.querySelector("#agregar-tarea");
+const $botonCerrar = document.querySelector("#btn-cerrar-modal");
+const $modalForm = document.querySelector("#agregar-tarea-modal");
+
